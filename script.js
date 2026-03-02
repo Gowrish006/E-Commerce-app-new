28fb89f91bbbc7dc4bb96d20c060b3671fb03796
root <root@ip-172-31-15-4.ap-south-2.compute.internal>
2026-03-02 05:05:03 +0000

    Added Gowrish E-Commerce static website UI

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function addToCart() {
-    alert("Item added to cart successfully!");
+function scrollToProducts(){
+  document.getElementById("products").scrollIntoView({
+    behavior:"smooth"
+  });
 }
+
+function buy(){
+  alert("Thank you for shopping with Gowrish E-Commerce 🛒");
+}
+
+/* Dark Mode */
+const toggle=document.getElementById("themeToggle");
+
+toggle.addEventListener("click",()=>{
+  document.body.classList.toggle("dark");
+
+  toggle.textContent =
+    document.body.classList.contains("dark") ? "☀️" : "🌙";
+});
